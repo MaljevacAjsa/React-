f00323a5c93cbc1faf7052a5b7e386d45eac8e58
Maljevac Ajsa <ajsamaljevac265@gmail.com>
2025-09-12 01:08:17 +0200

    Add initial App component with styling and Message integration

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Message from "./Message.jsx";
+
+// PascalCase
+
+function App() {
+  return (
+    <div style={{ backgroundColor: "#72d1ed" }}>
+      <h1>ajsaa</h1>
+      <Message />
+    </div>
+  );
+}
+
+export default App;
