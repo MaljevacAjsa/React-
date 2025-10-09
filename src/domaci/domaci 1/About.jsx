8b7a074b5e38e4ac8d4ad0f6d34be819364b317f
Maljevac Ajsa <ajsamaljevac265@gmail.com>
2025-10-09 17:15:18 +0200

    Add About component to introduce personal information and background

---
+++ src/domaci/domaci 1/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <p>
+      Ja sam Ajsa Maljevac, imam 20 godina. Studiram Softverski Inzenjering.{" "}
+      <br />
+      Imam narandzastu macku i zove se Teodor Delano Ruzvelt, Tedi.
+    </p>
+  );
+}
+
+export default About;
